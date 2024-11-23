aeb6b15dc09bbef1da1fdc59296f4c4fdcfba407
ika <thebroirakli@gmail.com>
2024-11-23 17:13:13 +0400

    fixed mouse look

--- Assets/Scripts/Aircraft/WeaponCamera.cs
+++ Assets/Scripts/Aircraft/WeaponCamera.cs
@@ -1,51 +1,34 @@
+using System;
 using UnityEngine;
 
 public class WeaponCamera : MonoBehaviour
 {
-    public enum RotationAxes
-    {
-        MouseXAndY = 0,
-        MouseX = 1,
-        MouseY = 2,
-    }
-
-    public RotationAxes axes = RotationAxes.MouseXAndY;
     public float sensitivityX = 15F;
     public float sensitivityY = 15F;
 
-    public float minimumX = -360F;
-    public float maximumX = 360F;
+    public float minimumX = -60f;
+    public float maximumX = 60f;
 
     public float minimumY = -60F;
     public float maximumY = 60F;
 
-    private Rigidbody rb;
     float rotationY = 0F;
+    float rotationX = 0F;
 
-    void Start() { }
+    void Start() 
+    {
+        rotationX = transform.localEulerAngles.y;
+        if (rotationX > 180) rotationX -= 360;
+    }
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX =
-                transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
     }
 }
